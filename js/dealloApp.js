2e9a6cc2f16684ecfaf5eb0ea0819d1c9ac423a2
SelenaYap <100066109@students.swinburne.edu.my>
2017-10-02 15:48:17 +0800

    Updated bootstrap and jquery files and added a JS file for AngularJS usage

---
+++ js/dealloApp.js
@@ -0,0 +1,16 @@
+/*jslint white:true */
+/*global angular */
+
+var app = angular.module('myApp', []);
+
+app.controller("myCtrl", function($scope){
+    'use strict';
+
+    //Edit Button
+    $scope.editFunc = function(){
+           alert("Status textbox is empty!");
+        }
+    };
+    
+}
+);
