d57047048349d72f9037fdd316cac3ff8af89c10
SelenaYap <100066109@students.swinburne.edu.my>
2017-10-02 17:40:27 +0800

    Finished Edit Profile UI portion

--- pages/profile.php
+++ pages/profile.php
@@ -16,14 +16,15 @@
     <![endif]--> 
 </head>
     
-    <body class="row" data-ng-app="myApp">
-        <div class="well text-center" data-ng-controller="myCtrl">
+    <body class="container" data-ng-app="myApp">
+        <div class="well text-center row" data-ng-controller="myCtrl">
             <h1>PROFILE</h1>
 
             <div id="profileDetails" data-ng-model="profileDetails" data-ng-show="!showEdit">
                 <img src="../images/DealloLogo.png" alt="profilePicture" class="img-circle" height="200px" width="200px"/>
                 <h2>Username</h2>
                 <h3 class="label label-success"><span class="glyphicon glyphicon-envelope"></span> Verified with e-mail</h3>
+                <br/><br/>
                 <p>Kuching, Sarawak</p>
 
                 <a href="#">News <span class="badge">5</span></a><br>
@@ -37,13 +38,27 @@
                 <img src="../images/DealloLogo.png" alt="profilePicture" class="img-circle" height="200px" width="200px"/>
                 <h2>EDIT PROFILE</h2>
                 <h3 class="label label-success"><span class="glyphicon glyphicon-envelope"></span> Verified with e-mail</h3>
-                <input type="text" class="form-control" placeholder="Username" size="10" max="10">
-                <input type="password" class="form-control" placeholder="Password" size="10" max="10">
-                <p>Kuching, Sarawak</p>
-
                 <br/><br/>
-                <p><button type="button" class="btn btn-primary" data-ng-click="showEdit=!showEdit">DONE EDITING<span class="glyphicon glyphicon-pencil"></span></button></p>
+                
+                <form action="" method="post" class="form-vertical">
+                    <div class="form-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span><input type="text" name="username" class="form-control" placeholder="Username" size="10" max="10">
+                        
+                        <input type="password" name="password" class="form-control" placeholder="Password" size="10" max="10">
+                       
+                        <input type="passwordTwice" name="passwordTwice" class="form-control" placeholder="Password, again" size="10" max="10">
+                        
+                        <input type="email" name="email" class="form-control" placeholder="E-mail address" size="20" max="20">
+                        
+                        <input type="cityCountry" name="cityCountry" class="form-control" placeholder="City,Country" size="10" max="10">
 
+                        <textarea class="form-control" rows="5" id="bio" name="bio" placeholder="Describe yourself here..."></textarea>
+
+                        <br/><br/>
+                        <p><button type="submit" class="btn btn-primary" data-ng-click="showEdit=!showEdit">DONE EDITING<span class="glyphicon glyphicon-pencil"></span></button></p>
+                    </div>
+                
+                </form>
             </div>
         </div>
     </body>
