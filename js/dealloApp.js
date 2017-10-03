e93a1455d17dc0f1058740e23359ebc9fe3f965b
SelenaYap <100066109@students.swinburne.edu.my>
2017-10-03 09:57:06 +0800

    Modified Angular features

--- js/dealloApp.js
+++ js/dealloApp.js
@@ -5,11 +5,8 @@ var app = angular.module('myApp', []);
 
 app.controller("myCtrl", function($scope){
     'use strict';
-    
-    
 
     //Edit Button
     $scope.editFunc = function(){
-        alert("HELLO WORLD");
     };
 });
