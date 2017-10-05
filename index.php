f476195985f1e5bd59c38a3dd149e6c85a478a2f
SelenaYap <100066109@students.swinburne.edu.my>
2017-10-05 11:44:56 +0800

    Added internet explorer 8 Suppot for HTML5

--- index.php
+++ index.php
@@ -34,6 +34,15 @@
 
     <!-- Custom styles for this template -->
   <link href="styles/style.css" rel="stylesheet">
+  
+      <!-- HTML5 Shim and Respond.js IE8 support of HTML5 
+    elements and media queries --> 
+    <!-- WARNING: Respond.js doesn't work if you view the 
+    page via file:// --> 
+    <!--[if lt IE 9]> 
+    <script src="js/html5shiv.js"></script> 
+    <script src="js/respond.min.js"></script> 
+    <![endif]--> 
 
   </head>
 
