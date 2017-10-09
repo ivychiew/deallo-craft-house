d3dd9b648f241770844e2413a2d0ef0a962f928e
ivychiew <4334787@students.swinburne.edu.my>
2017-10-09 16:10:00 +0800

    Fixed the alignment for the text and images in product pages

---
+++ js/buttonToggle.js
@@ -0,0 +1,10 @@
+// Javascript for Element Toggle 
+
+function buttonToggle() {
+    var x = document.getElementById("Description");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
--- pages/products.php
+++ pages/products.php
@@ -40,9 +40,12 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
 <title>Products</title>
+
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+ 
 </head>
 
 <body>
@@ -68,12 +71,12 @@
             <!-- <p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p> -->
            <!--  <p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p> -->
          
-                <li class="nav-item">
-                  <a class="nav-link" href="index.php?logout='1'" style="color: red;">Sign out</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="pages/profile.php">Edit Profile</a>
-                </li>
+             <li class="nav-item">
+               <a class="nav-link" href="index.php?logout='1'" style="color: red;">Sign out</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link" href="pages/profile.php">Edit Profile</a>
+             </li>
 
              <?php endif ?>
             <li class="nav-item">
@@ -111,40 +114,70 @@
 
 	<div class="page-header">
 		<br>
-    	<h1 class="h2">All Products <a class="btn btn-default" href="addproducts.php"> <span class="glyphicon glyphicon-plus"></span> &nbsp; Create a new product </a></h1> 
+    	<h1 class="h2">All Products 
+    		<a class="btn btn-default" href="addproducts.php"> 
+    			<span class="glyphicon glyphicon-plus"></span> &nbsp; Create a new product 
+    		</a>
+    	</h1> 
     </div>
     
 <br />
 
+
+
 <div class="row">
 <?php
 	
+	//Fetch the data from the database
 	$stmt = $DB_con->prepare('SELECT productID, productName, productPrice, productPic, productDescription FROM product_tbl ORDER BY productID DESC');
 	$stmt->execute();
 	
+	//If the number of products is more than 0 
 	if($stmt->rowCount() > 0)
 	{
+		//Fetch the products from the database table to a row
 		while($row=$stmt->fetch(PDO::FETCH_ASSOC))
-		{
+		{	
+			//Extract data to a row
 			extract($row);
 			?>
 			<div class="col-xs-3">
+
+				<!--Product Name-->
 				<h3 class="page-header" align="center"><?php echo $productName ?></h3>
-				<img src="../images/product_images/<?php echo $row['productPic']; ?>" class="img-rounded" width="250px" height="250px" />
+				<!--Product Image-->
+				<img src="../images/product_images/<?php echo $row['productPic']; ?>" align="middle" class="img-rounded" width="250px" height="250px" />
+				<!--Product Price-->
 				<p class="page-header" align="center"><?php echo $productPrice; ?></p>
-				<p align="center"><?php echo $productDescription; ?></p>
+				<!--Product Description-->
+				<!-- <p align="center"> --><!-- <?php echo $productDescription; ?></p> -->
 
+				<!--Buttons-->
 
-				<span>
-					<a class="btn btn-info" href="editform.php?edit_id=<?php echo $row['productID']; ?>" title="click for edit" onclick="return confirm('sure to edit ?')"> <span class="glyphicon glyphicon-edit"></span> Edit</a> 
+				<span> <!--Edit Product Button-->
+					<a class="btn btn-info" href="editform.php?edit_id=<?php echo $row['productID']; ?>" title="click for edit" onclick="return confirm('sure to edit ?')">
+						<span class="glyphicon glyphicon-edit"></span> Edit
+					</a> 
 
-					<a class="btn btn-danger" href="?delete_id=<?php echo $row['productID']; ?>" title="click for delete" onclick="return confirm('Are you sure ?')"><span class="glyphicon glyphicon-remove-circle">
-				</span> Delete</a>
-
-					<a class="btn btn-info" href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Add to Cart </a>
+						<!--Delete Product Button-->
+					<a class="btn btn-danger" href="?delete_id=<?php echo $row['productID']; ?>" title="click for delete" onclick="return confirm('Are you sure ?')">
+						<span class="glyphicon glyphicon-remove-circle"></span> Delete
+					</a>
 				</span>
-				</p>
+
+				<!--Add to Cart Button-->
+					 <!-- <a class="btn btn-info" href="#">
+						<span class="glyphicon glyphicon-shopping-cart"></span> Add to Cart
+					</a> -->
+				
+					<!--Show Description Button-->
+					<script type='text/javascript' src='../js/buttonToggle.js'></script>
+					<a class="btn btn-danger" title="click for more info" onclick="buttonToggle()"> More Info</a>
+					
+				
+				
 			</div> 
+
 
 			<?php
 		}
@@ -152,6 +185,8 @@
 	else
 	{
 		?>
+
+		<!--If Empty Data, Show no data found-->
         <div class="col-xs-12">
         	<div class="alert alert-warning">
             	<span class="glyphicon glyphicon-info-sign"></span> &nbsp; No Data Found ...
@@ -162,19 +197,21 @@
 	
 ?>
 </div>	
-
-
-<!-- 
-<div class="alert alert-info">
-    <strong>tutorial link !</strong> <a href="http://www.codingcage.com/2016/02/upload-insert-update-delete-image-using.html">Coding Cage</a>!
+<div class="row">
+	<div class="col-xs-3 col-md-3 col-lg-3">
+	<br>
+		<div id="Description" style="display:none;">
+						<?php echo $productDescription; ?>
+					</div>
+	</div>
 </div>
- -->
+<!--End of container--> 
 </div>
 
 
 <!-- Latest compiled and minified JavaScript -->
-<script src="bootstrap/js/bootstrap.min.js"></script>
-
+<script src="../js/bootstrap.min.js"></script>
+<script type='text/javascript' src='../js/buttonToggle.js'></script>
 
 </body>
 </html>
