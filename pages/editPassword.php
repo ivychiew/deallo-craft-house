6db4cf49dfd780cae977ba92abca6bcef17e0772
SelenaYap <100066109@students.swinburne.edu.my>
2017-10-11 23:19:36 +0800

    Tested page and minor bootstrap styling added

--- pages/editPassword.php
+++ pages/editPassword.php
@@ -2,10 +2,10 @@
 <html lang="en">
 <head>
     <title>Deallo Reset Password</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     
     <link rel="stylesheet" href="../styles/bootstrap/bootstrap.css">
     <link rel="stylesheet" href="../styles/bootstrap/bootstrap.css.min">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" /> 
     <link href="../styles/style.css" rel="stylesheet" type="text/css">
     <link rel="icon" type="image/png" href="../images/DealloLogo-favicon.png"/> 
 
