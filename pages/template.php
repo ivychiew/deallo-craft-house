442d488b06688bda2489c59f3ed1c0f630178ee6
SelenaYap <100066109@students.swinburne.edu.my>
2017-10-16 16:00:06 +0800

    Added template to draft similar navigations and footers

---
+++ pages/template.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" /> 
+	
+    <title>Deallo Craft House</title>
+	<!-- Custom styles for this template -->
+	<link href="..\styles\bootstrap/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<link href="..\styles\bootstrap\bootstrap.css" rel="stylesheet" type="text/css">
+    <link rel="icon" type="image/png" href="../images/DealloLogo-favicon.png"/> 
+    <link href="..\styles\bootstrap\bootstrap.css" rel="stylesheet" type="text/css">
+	
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 
+    elements and media queries --> 
+    <!-- WARNING: Respond.js doesn't work if you view the 
+    page via file:// --> 
+    <!--[if lt IE 9]> 
+    <script src="js/html5shiv.js"></script> 
+    <script src="js/respond.min.js"></script> 
+    <![endif]--> 
+</head>
+        
+<body class="container-fluid">
+
+	
+</body>
+    
+    
+    
+        
+    <footer class="py-5 bg-dark">
+    <div class="container">
+            <p class="m-0 text-center text-white col-md-12 col-sm-12 col-lg-12">Copyright &copy; Deallo's Craft House</p>
+      </div>
+    </footer>
+    
+    <!-- jQuery – required for Bootstrap's JavaScript plugins) --> 
+    <script src="../js/jquery.min.js"></script> 
+    <!-- All Bootstrap  plug-ins  file --> 
+    <script src="../js/bootstrap.min.js"></script> 
+    <!-- Basic AngularJS --> 
+    <script src="../js/angular.min.js"></script> 
+    <!-- AngularJS - Routing --> 
+    <script src="../js/angular-route.min.js"></script>
+    
+</html>
