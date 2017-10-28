cda2a575393d454ba585ec194bd73bea3cb1534c
SelenaYap <100066109@students.swinburne.edu.my>
2017-10-28 14:53:24 +0800

    Added basic (incomplete) Gmail login functions

--- pages/login.php
+++ pages/login.php
@@ -5,6 +5,17 @@
 	<title>Deallo Craft House - Login</title>
 	<link rel="stylesheet" type="text/css" href="..\styles\login_styles.css"/>
     <link rel="icon" type="image/png" href="images/DealloLogo-favicon.png"/>
+    <script src="../functions/facebookapi.js"></script>
+	<script src="https://apis.google.com/js/platform.js" async defer></script>
+	<meta name="google-signin-client_id" content="629649200313-68ffuflofdj0emikqshht7lgq003jcg5.apps.googleusercontent.com">
+	<script>function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}</script>
+   
 </head>
 <body class="bg">
 
@@ -28,6 +39,21 @@
 		<div class="input-group">
 			<button type="submit" class="btn" name="login_user">Login</button>
 		</div>
+		
+		<div class="g-signin2" data-onsuccess="onSignIn"></div>
+        
+        <div
+    class="fb-login-button"
+    data-max-rows="1"
+    data-size="<medium, large>"
+    data-button-type="continue_with"
+    data-width="<100% or px>"
+    data-scope="<comma separated list of permissions, e.g. public_profile, email>"
+></div>
+
+        <div id="status">
+        </div>
+        
 		<p>
 			Not yet a member? <a href="register.php">Sign up</a>
 		</p>
